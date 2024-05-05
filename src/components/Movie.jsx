c2fac531fdcc9a2e43b6c27af963dad3fabd92dc
Torycd <vicidundun@gmail.com>
2024-05-05 17:27:02 +0100

    lets go

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 const Movie = ({ item }) => {
-//   const [like, setLike] = useState(false);
-//   setLike(false)
+  const [like, setLike] = useState(false);
+
   return (
     <div className="w-[160px] sn:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
       <img
@@ -15,13 +15,12 @@ const Movie = ({ item }) => {
         <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
           {item?.title}
         </p>
-        <p>
-          {/* {like ? (
+        <p onClick={() => setLike(false)}>
+          {like ? (
             <FaHeart className="absolute top-4 left-4 text-gray-300" />
           ) : (
             <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
-          )} */}
-          <FaHeart/>
+          )}
         </p>
       </div>
     </div>
