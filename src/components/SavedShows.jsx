719127b35483786efafed1aa2ef4f7c0adef366d
Torycd <vicidundun@gmail.com>
2024-05-07 11:26:04 +0100

    done

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -1,23 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { UserAuth } from "../context/authContext";
 import { db } from "../firebase";
-import {updateDoc, doc, onSnapShot} from 'firebase/firestore'
+import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SavedShows = () => {
-    const {user} = UserAuth()
-    const slideLeft = () => {
-        var slider = document.getElementById("slider");
-        slider.scrollLeft = slider.scrollLeft - 500;
-      };
-      const slideRight = () => {
-        var slider = document.getElementById("slider");
-        slider.scrollLeft = slider.scrollLeft + 500;
-      };
+  const [movies, setMovies] = useState([]);
+  const { user } = UserAuth();
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  useEffect(() => {
+    onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
+      setMovies(doc.data()?.savedShows);
+    });
+  }, [user?.email]);
+
+  const MovieRef = doc(db, "users", `${user?.email}`);
+  const deleteShow = async (passedId) => {
+    try {
+      const result = movies.filter((item) => item.id !== passedId)
+      await updateDoc(MovieRef, {
+        savedShows: result
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-      <h2 className="text-white fot-bold md:text-xl p-4">My Shows</h2>
+      <h2 className="text-white font-bold md:text-xl p-4">My Shows</h2>
       <div className="relative flex items-center group">
         <MdChevronLeft
           onClick={slideLeft}
@@ -29,19 +48,27 @@ const SavedShows = () => {
           className="w-full h-full left-0 whitespace-nowrap overflow-x-scroll scroll-smooth scrollbar-hide relative"
         >
           {movies.map((item, id) => (
-            <div className="w-[160px] sn:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-            <img
-              className="w-full h-auto block"
-              src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
-              alt={item?.title}
-            />
-            <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-              <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                {item?.title}
-              </p>
-              
+            <div
+              key={id}
+              className="w-[160px] sn:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
+            >
+              <img
+                className="w-full h-auto block"
+                src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
+                alt={item?.title}
+              />
+              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+                  {item?.title}
+                </p>
+                <p
+                  onClick={() => deleteShow(item.id)}
+                  className="absolute text-gray-300 top-4 right-4"
+                >
+                  <AiOutlineClose />
+                </p>
+              </div>
             </div>
-          </div>
           ))}
         </div>
         <MdChevronRight
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import SavedShows from "../components/SavedShows";
 
 const Account = () => {
   return (
     <>
       <div className="w-full text-white">
         <img
-          className="absolute w-full h-[400px] object-cover"
+          className="w-full h-[400px] object-cover"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/d253acf4-a1e2-4462-a416-f78802dc2d85/313e6dde-c3f6-4ddb-a41e-96cddd055ee9/NG-en-20240429-POP_SIGNUP_TWO_WEEKS-perspective_WEB_522b8935-f334-480e-ab1f-883eb9d353a5_large.jpg"
           alt="/"
         />
@@ -14,6 +15,7 @@ const Account = () => {
           <h1 className="text-3xl md:text-5xl font-bold">My shows</h1>
         </div>
       </div>
+      <SavedShows/>
     </>
   );
 };
